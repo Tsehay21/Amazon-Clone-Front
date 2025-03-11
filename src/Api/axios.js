77be11718f02a328f4d1a43b55562a2714caa40a
Tsehay21 <tsehay1900@yahoo.com>
2025-03-10 22:55:25 -0400

    axios

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,8 +2,14 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
+  //local instance of firebase functions
   // baseURL: "http://127.0.0.1:5001/clone-2c6a8/us-central1/api",
+  
+  //deployed version of firebase function
   baseURL: "https://api-fxatoytyya-uc.a.run.app",
+
+  //deployed version of amazon server on render.com
+  // baseURL: "https://amazon-api-deploy-xvdr.onrender.com/",
 });
 
 
